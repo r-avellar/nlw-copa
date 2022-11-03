13dac881a9541fe3df253ce152b17cf439234e6f
r-avellar <renatoavellar5@gmail.com>
2022-11-03 00:53:26 -0300

    criando rotas da aplicação e gerando seeds

---
+++ server/prisma/seed.ts
@@ -0,0 +1,62 @@
+import { PrismaClient } from '@prisma/client'
+
+const prisma = new PrismaClient()
+
+async function main(){
+    const user = await prisma.user.create({
+        data: {
+            name: 'John Doe',
+            email: 'john.doe@gmail.com',
+            avatarURL: 'https://github.com/r-avellar.png'
+        }
+    })
+
+    const pool = await prisma.pool.create({
+        data:{
+            title: 'Example Pool',
+            code: 'BOL123',
+            ownerId: user.id,
+
+            participants: {
+                create:{
+                    userId: user.id
+                }
+            }
+        }
+    })
+    
+    await prisma.game.create({
+        data: {
+            date: '2022-11-28T13:00:00.276Z',
+            firstTeamCountryCode: 'BR',
+            secondTeamCountryCode: 'CH'
+        }
+    })
+
+    await prisma.game.create({
+        data: {
+            date: '2022-11-24T16:00:00.276Z',
+            firstTeamCountryCode: 'BR',
+            secondTeamCountryCode: 'RS',
+
+            guesses: {
+                create:{
+                    firstTeamPoints: 3,
+                    secondTeamPoints: 1,
+
+                    participant: {
+                        connect:{
+                            userId_poolId:{
+                                userId: user.id,
+                                poolId: pool.id
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    })
+
+}
+
+main()
