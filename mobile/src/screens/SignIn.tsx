989b4abfe0cd7465b1d952204148f6eb9f7d667a
r-avellar <renatoavellar5@gmail.com>
2022-11-04 13:07:28 -0300

    finalizando tela de login

--- mobile/src/screens/SignIn.tsx
+++ mobile/src/screens/SignIn.tsx
@@ -1,11 +1,24 @@
-import { Center, Text} from 'native-base'
+import { Center, Text, Icon} from 'native-base'
+import Logo from '../assets/logo.svg'
+import { Button } from '../components/button'
+import { Fontisto} from '@expo/vector-icons'
 
 export function SignIn(){
     return(
-        <Center flex={1} bgColor="gray.900">
-            <Text color="white" fontSize={24} fontFamily="heading" >
-                Sign in
+        <Center flex={1} bgColor="gray.900" p={7}>
+           <Logo width={212} height={40}/>
+
+           <Button 
+                type="SECONDARY" 
+                title="ENTRAR COM O GOOGLE" 
+                leftIcon={ <Icon as={Fontisto} name="google" color="white" size="md" />}
+                mt={12}
+            />
+
+            <Text color="white" textAlign="center" mt={4}>
+                Não utilizamos nenhuma informação além { '\n'}
+                do seu e-mail para a criação da sua conta.
             </Text>
-      </Center>
+        </Center>
     )
 }
